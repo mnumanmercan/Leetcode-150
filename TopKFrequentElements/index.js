e83e137238576caacd4db78cee00defc6b80f7d3
Numan <numan.mercan24@gmail.com>
2024-03-06 15:28:53 +0300

    topK frequent elements

--- TopKFrequentElements/index.js
+++ TopKFrequentElements/index.js
@@ -1,6 +1,9 @@
 var topKFrequent = function (nums, k) {
+    if(nums.length == 1){
+        return nums;
+    }
     let frequent = {};
-
+    let resultArr = [];
     for(let i = 0; i < nums.length; i++) {
         if(frequent[nums[i]]){
             frequent[nums[i]] += 1;
@@ -8,7 +11,12 @@ var topKFrequent = function (nums, k) {
             frequent[nums[i]] = 1;
         }
     }
-    console.log(Object.keys(frequent))
+    frequent = Object.entries(frequent).sort((a, b) => b[1] - a[1])
+    for(let j = 0; j<k; j++){
+        resultArr.push(Number(frequent[j][0]))
+    }
+
+    return resultArr;
 };
 
-console.log(topKFrequent([1,1,1,2,2,3], 2));
+console.log(topKFrequent([1,2,2,3,4,3,4], 2));
